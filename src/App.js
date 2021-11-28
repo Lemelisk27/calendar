4f0150604136995f926d8fa740143c6b73b30697
Zach Smith <Lemelisk27@gmail.com>
2021-11-28 02:43:33 -0800

    created a calendar with a tooltip

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, {Fragment} from "react";
 import {BrowserRouter as Router,Routes,Route,Navigate} from "react-router-dom"
 import Calendar from "./calendar";
 import Datenav from "./datenav"
+import Tooltip from "./tooltip"
 
 function App() {
   return (
     <Router>
       <Fragment>
         <Routes>
-          <Route exact path="/" element={<Navigate to="/viewswitch"/>}/>
+          <Route exact path="/" element={<Navigate to="/tooltip"/>}/>
           <Route exact path="/viewswitch" element={<Calendar />}/>
           <Route exact path="/datenav" element={<Datenav />}/>
-          <Route path="*" element={<Navigate to="/viewswitch"/>}/>
+          <Route exact path="/tooltip" element={<Tooltip />}/>
+          <Route path="*" element={<Navigate to="/tooltip"/>}/>
         </Routes>
       </Fragment>
     </Router>
