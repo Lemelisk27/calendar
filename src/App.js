0f9e1c479e1907896fac1d8d62f1060ed06b2c35
Zach Smith <Lemelisk27@gmail.com>
2021-11-28 02:22:51 -0800

    testing the date navigator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {Fragment} from "react";
+import {BrowserRouter as Router,Routes,Route,Navigate} from "react-router-dom"
 import Calendar from "./calendar";
+import Datenav from "./datenav"
 
 function App() {
   return (
-    <div className="App">
-      <Calendar />
-    </div>
+    <Router>
+      <Fragment>
+        <Routes>
+          <Route exact path="/" element={<Navigate to="/viewswitch"/>}/>
+          <Route exact path="/viewswitch" element={<Calendar />}/>
+          <Route exact path="/datenav" element={<Datenav />}/>
+          <Route path="*" element={<Navigate to="/viewswitch"/>}/>
+        </Routes>
+      </Fragment>
+    </Router>
   );
 }
 
