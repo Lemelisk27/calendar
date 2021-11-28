e219bc81c59e053dcd9b3b635c1c2e927c259a87
Zach Smith <Lemelisk27@gmail.com>
2021-11-28 15:24:45 -0800

    added edit appt with custom field

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import {BrowserRouter as Router,Routes,Route,Navigate} from "react-router-dom"
 import Calendar from "./calendar";
 import Datenav from "./datenav"
 import Tooltip from "./tooltip"
+import Edit from "./edit"
 
 function App() {
   return (
     <Router>
       <Fragment>
         <Routes>
-          <Route exact path="/" element={<Navigate to="/tooltip"/>}/>
+          <Route exact path="/" element={<Navigate to="/edit"/>}/>
           <Route exact path="/viewswitch" element={<Calendar />}/>
           <Route exact path="/datenav" element={<Datenav />}/>
           <Route exact path="/tooltip" element={<Tooltip />}/>
-          <Route path="*" element={<Navigate to="/tooltip"/>}/>
+          <Route exact path="/edit" element={<Edit />}/>
+          <Route path="*" element={<Navigate to="/edit"/>}/>
         </Routes>
       </Fragment>
     </Router>
