565304d23217b7500cbd9ad0d74ebc32d00db56e
Zach Smith <Lemelisk27@gmail.com>
2021-11-27 22:57:33 -0800

    day, week, month calendar working

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -1,35 +1,86 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
   DayView,
+  WeekView,
+  MonthView,
   Appointments,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
-function Calendar () {
-    const currentDate = '2018-11-01';
-    const schedulerData = [
-        { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-        { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-    ];
+import { appointments } from "../calendar/demo-data/month-appointments"
 
-    return (
-        <Paper>
-            <Scheduler
-                data={schedulerData}
-            >
-                <ViewState
-                    currentDate={currentDate}
+const ExternalViewSwitcher = ({
+    currentViewName,
+    onChange,
+}) => (
+    <RadioGroup
+        aria-label="views"
+        style={{ flexDirection: "row" }}
+        name="views"
+        value={currentViewName}
+        onChange={onChange}
+    >
+        <FormControlLabel value="Day" control={<Radio />} label="Day" />
+        <FormControlLabel value="Week" control={<Radio />} label="Week" />
+        <FormControlLabel value="Work Week" control={<Radio />} label="Work Week" />
+        <FormControlLabel value="Month" control={<Radio />} label="Month" />
+    </RadioGroup>
+)
+
+export default class Calendar extends React.PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: appointments,
+            currentViewName: 'Month'
+        }
+        this.currentViewNameChange = (e) => {
+            this.setState({ currentViewName: e.target.value })
+        }
+    }
+
+    render() {
+        const { data, currentViewName } = this.state
+
+        return (
+            <React.Fragment>
+                <ExternalViewSwitcher
+                    currentViewName={currentViewName}
+                    onChange={this.currentViewNameChange}
                 />
-                <DayView
-                    startDayHour={9}
-                    endDayHour={14}
-                />
-                <Appointments />
-            </Scheduler>
-        </Paper>
-    )
+                <Paper>
+                    <Scheduler
+                        data={data}
+                        height={660}
+                    >
+                        <ViewState
+                            defaultCurrentDate="2018-07-25"
+                            currentViewName={currentViewName}
+                        />
+                        <DayView
+                            startDayHour={10}
+                            endDayHour={19}
+                        />
+                        <WeekView
+                            startDayHour={10}
+                            endDayHour={19}
+                        />
+                        <WeekView
+                            name="Work Week"
+                            excludedDays={[0,6]}
+                            startDayHour={9}
+                            endDayHour={19}
+                        />
+                        <MonthView />
+                        <Appointments />
+                    </Scheduler>
+                </Paper>
+            </React.Fragment>
+        )
+    }
 }
-
-export default Calendar
